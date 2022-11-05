403eb167beb53a28ec86c6c9fd6fc612abde1d5b
m-oka-system <fun-for-fan@outlook.com>
2022-11-05 05:42:49 +0000

    [update]サブスクリプションIDの変数を修正

--- azurecli/create-sp.sh
+++ azurecli/create-sp.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 # Required azure cli version >= 2.25.0
 az ad sp create-for-rbac --role "Contributor" \
   --name "azure_terraform" \
-  --scopes "/subscriptions/${azure_subscription_id}"
+  --scopes "/subscriptions/${ARM_SUBSCRIPTION_ID}"
