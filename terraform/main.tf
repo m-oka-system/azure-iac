00411f018b4ef71bf0f6740169e6c8a5f1ed2380
m-oka-system <fun-for-fan@outlook.com>
2022-11-07 11:21:42 +0000

    [clean]providerとリソースグループをmain.tfへ統合

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "http" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-${var.env}-rg"
+  location = var.location
+}
+
+module "vm" {
+  source = "./modules/vm"
+
+  count               = local.create_count
+  prefix              = var.prefix
+  env                 = var.env
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  public_subnet_id    = azurerm_subnet.public.id
+  vm_size             = var.vm_size
+  vm_admin_username   = var.vm_admin_username
+  vm_admin_password   = var.vm_admin_password
+  source_image_id     = data.azurerm_image.win2022_ja.id
+}
--- terraform/resourcegroup.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "network_rg" {
-  name     = "${var.prefix}-network-rg"
-  location = var.location
-}
-
-resource "azurerm_resource_group" "server_rg" {
-  name     = "${var.prefix}-server-rg"
-  location = var.location
-}
