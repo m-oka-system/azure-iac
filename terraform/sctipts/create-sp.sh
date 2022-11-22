55dd8006332d24f0733ed8a9786d3bb2e2979ce2
m-oka-system <m-oka-system@outlook.com>
2022-11-22 01:10:20 +0000

    [update]ユーザーアクセス管理者の割り当てを追加

--- terraform/sctipts/create-sp.sh
+++ terraform/sctipts/create-sp.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Required azure cli version >= 2.25.0
-az ad sp create-for-rbac --role "Contributor" \
-  --name "azure_terraform" \
-  --scopes "/subscriptions/${ARM_SUBSCRIPTION_ID}"
+# # Required azure cli version >= 2.25.0
+# az ad sp create-for-rbac --role "Contributor" \
+#   --name "azure_terraform" \
+#   --scopes "/subscriptions/${ARM_SUBSCRIPTION_ID}"
+
+# Create service principal
+appId=$(az ad sp create-for-rbac --name "azure_terraform" --query "appId" --output tsv)
+
+# Assign roles
+az role assignment create --assignee "$appId" \
+  --role "Contributor" \
+  --subscription "${ARM_SUBSCRIPTION_ID}"
+
+az role assignment create --assignee "$appId" \
+  --role "User Access Administrator" \
+  --subscription "${ARM_SUBSCRIPTION_ID}"
